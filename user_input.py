0e8c1a803abab8c6ae84eb9b6d6ec1eb06db224c
scushi963 <scolahwanjiku963@gmail.com>
2024-08-22 17:16:55 +0300

    INTRODUCTION TO PYTHON

---
+++ user_input.py
@@ -0,0 +1,15 @@
+#Asking for the user's name 
+My_name = "Scolah"
+print(f"Helloo! What is your name? {My_name}")
+
+#Asking for the user's age
+My_age = 21
+print(f"How old are you? {My_age}")
+
+#Asking for the user's location
+My_location = "Nairobi"
+print(f"Where do you live? {My_location}")
+
+#Printing out a personalized message using the user's info
+print(f"Hi {My_name}, so you said you are {My_age} years old and you live in {My_location}, right. I'm really glad to have met you . I wish you all the best girl .")
+
